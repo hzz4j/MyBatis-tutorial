253f56850408917a8ee98d0706366c6c3d2dc41a
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-29 16:24:13 +0800

    添加用挂

--- 01 First-Start/src/test/java/edu/cau/UserTest.java
+++ 01 First-Start/src/test/java/edu/cau/UserTest.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,25 +12,45 @@ import java.io.InputStream;
 import java.util.List;
 
 public class UserTest {
+    private SqlSessionFactory sqlSessionFactory = null;
 
-    @Test
-    public void getUsers(){
+    @Before
+    public void init(){
         String resource = "mybatis-config.xml";
         try {
             InputStream inputStream = Resources.getResourceAsStream(resource);
 
-            SqlSessionFactory sqlSessionFactory =
+            sqlSessionFactory =
                     new SqlSessionFactoryBuilder().build(inputStream);
-
-            SqlSession sqlSession = sqlSessionFactory.openSession();
-
-            List<User> users = sqlSession.selectList("userMapper.selectAllUsers");
-
-            System.out.println(users);
-            users.stream().forEach(System.out::println);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void getUsers(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<User> users = sqlSession.selectList("userMapper.selectAllUsers");
+
+        System.out.println(users);
+        users.stream().forEach(System.out::println);
+    }
+
+    // 插入一条数据
+    @Test
+    public void insertUser(){
+        User user = new User();
+        user.setId(2);
+        user.setUsername("Q10Viking");
+        user.setPassword("123456");
+
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        int result = sqlSession.insert("userMapper.save", user);
+
+        sqlSession.commit();
+        System.out.println("插入成功"+result);
+
     }
 }
