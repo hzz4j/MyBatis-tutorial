6d77e8b3645fed73e3e3b33d6f312538fd2c5e70
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-29 19:47:44 +0800

    添加typehandler

--- 03 Dynamic SQL/src/main/java/edu/cau/UserMapper.java
+++ 03 Dynamic SQL/src/main/java/edu/cau/UserMapper.java
@@ -10,6 +10,4 @@ public interface UserMapper {
     void save(User user);
 
     void deleteUser(Integer userId);
-
-
 }
---
+++ 04 TypeHandler/src/test/java/edu/cau/UserTest.java
@@ -0,0 +1,64 @@
+package edu.cau;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+
+public class UserTest {
+    private SqlSessionFactory sqlSessionFactory = null;
+
+    @Before
+    public void init(){
+        String resource = "mybatis-config.xml";
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+
+            sqlSessionFactory =
+                    new SqlSessionFactoryBuilder().build(inputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void saveUser(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = new User();
+        user.setUsername("Lizhi");
+        user.setPassword("123456");
+        user.setLevel(Level.HIGHT);
+
+        userMapper.save(user);
+        sqlSession.commit();
+    }
+
+
+    @Test
+    public void getUser(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        List<User> users = userMapper.selectUsers();
+        users.forEach(System.out::println);
+    }
+}
+/**
+ User{id=1, username='hello', password='mybatis', level=LOW}
+ User{id=4, username='Q10Viking', password='123456', level=HIGHT}
+ User{id=5, username='Q10Viking', password='123456', level=MEDIUM}
+ User{id=7, username='Lizhi', password='123456', level=HIGHT}
+ */
