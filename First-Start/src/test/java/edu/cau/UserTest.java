5e5f2dfc1e29859ad7114c18618433b1bb04e426
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-29 14:00:13 +0800

    mybatis-第一次传查询

---
+++ First-Start/src/test/java/edu/cau/UserTest.java
@@ -0,0 +1,34 @@
+package edu.cau;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class UserTest {
+
+    @Test
+    public void getUsers(){
+        String resource = "mybatis-config.xml";
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+
+            SqlSessionFactory sqlSessionFactory =
+                    new SqlSessionFactoryBuilder().build(inputStream);
+
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+
+            List<User> users = sqlSession.selectList("userMapper.selectAllUsers");
+            System.out.println(users);
+            users.stream().forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
