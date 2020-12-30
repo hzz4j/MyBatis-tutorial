b1b0b0557ef6461e2452e0fb28ce9acbb1a72825
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-30 15:13:38 +0800

    add itemMapper

---
+++ 09 Maven SSM/src/main/java/edu/cau/dao/ItemMapper.java
@@ -0,0 +1,8 @@
+package edu.cau.dao;
+
+import edu.cau.pojo.Item;
+
+
+public interface ItemMapper {
+    Item findItem(int id);
+}
---
+++ 09 Maven SSM/src/main/java/edu/cau/pojo/Item.java
@@ -0,0 +1,62 @@
+package edu.cau.pojo;
+
+import java.util.Date;
+
+public class Item {
+    private int id;
+    private String name;
+    private float price;
+    private Date createTime;
+    private String detail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", createTime=" + createTime +
+                ", detail='" + detail + '\'' +
+                '}';
+    }
+}
